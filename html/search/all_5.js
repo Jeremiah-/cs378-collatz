ed4b9b6513b6714493be106c50029ed49e4d453c
Jeremiah Martinez <jeremiah@cs.utexas.edu>
2015-06-17 21:29:43 -0500

    closes #10. closes #9. Adding the log and html/

---
+++ html/search/all_5.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['using_5farray',['USING_ARRAY',['../Collatz_8c_09_09.html#a7967d12be580a139e605a7dc20c3582b',1,'USING_ARRAY():&#160;Collatz.c++'],['../SphereCollatz_8c_09_09.html#a7967d12be580a139e605a7dc20c3582b',1,'USING_ARRAY():&#160;SphereCollatz.c++']]]
+];
---
+++ html/search/variables_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['cycle_5fcache',['cycle_cache',['../Collatz_8c_09_09.html#a946e936c9da3f0005b3701ec67cd7cec',1,'cycle_cache():&#160;Collatz.c++'],['../SphereCollatz_8c_09_09.html#a946e936c9da3f0005b3701ec67cd7cec',1,'cycle_cache():&#160;SphereCollatz.c++']]]
+];
